4781d7c4669b6a586eddaa3b22483edcd081153a
yk38spage <103328377+yk38spage@users.noreply.github.com>
2024-03-29 16:33:43 +0300

    Update navbar.js

--- kur-notu-hesap/src/js/navbar.js
+++ kur-notu-hesap/src/js/navbar.js
@@ -1,10 +1,10 @@
-// This file is part of Kur Notu Hesap.
+// This file is part of yk38spage.github.io.
 //
-// Kur Notu Hesap is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// yk38spage.github.io is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 //
-// Kur Notu Hesap is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// yk38spage.github.io is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along with Kur Notu Hesap. If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License along with yk38spage.github.io. If not, see <https://www.gnu.org/licenses/>.
 
 function openNav() {
     document.getElementById("nav-container").style.right = "0%";
